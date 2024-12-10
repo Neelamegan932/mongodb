7cf89d9152265c408da07470db023ee072cfa749
Neelamegan932 <neelamegan@3roses.tech>
2024-12-10 14:53:43 +0530

    route structure

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ const port = 8000;
 app.get("/", (req, res) => {
   res.json({ message: "Testing express" });
 });
+//CRUD FUNCTIONALITY METHODS
+//creating
+app.post("/movies", () => {});
+
+//reading
+app.get("/movies", () => {});
+
+//updating
+app.put("/movies/:id", () => {});
+
+//deleting
+app.delete("/movies/:id", () => {});
+
 app.listen(port, () => {
   console.log(`listening to http://localhost:${port}`);
 });
