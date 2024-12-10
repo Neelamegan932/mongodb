d780a54c9186dea3d402a718cf1e4dee21b45545
Neelamegan932 <neelamegan@3roses.tech>
2024-12-10 15:28:17 +0530

    route structure

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,16 +2,25 @@ import express from "express";
 const router = express.Router();
 
 //CRUD FUNCTIONALITY METHODS
+
 //creating
-router.post("/movies", () => {});
+router.post("/", (req, res) => {
+  res.send("Create movie lists");
+});
 
 //reading
-router.get("/movies", () => {});
+router.get("/", (req, res) => {
+  res.send("Get movie lists");
+});
 
 //updating
-router.put("/movies/:id", () => {});
+router.put("/:id", (req, res) => {
+  res.send("Update a movie");
+});
 
 //deleting
-router.delete("/movies/:id", () => {});
+router.delete("/:id", (req, res) => {
+  res.send("Delete a movie");
+});
 
 export default router;
